2133dfce95b369e9375dacd23f7725e1ad62fd64
Jakub Nowak <79985746+jakubno3ak@users.noreply.github.com>
2025-12-13 20:49:19 +0000

    New session

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -4,7 +4,6 @@ from settings import Settings
 class TestSettings:
     def test_settings_default_values(self):
         settings = Settings()
-        assert settings.s3_bucket_name == "mlops-lab11-models-kubano"
         assert settings.model_path == "model/sentence_transformer.model"
         assert settings.classifier_path == "model/classifier.joblib"
         assert settings.onnx_model_path == "model"
